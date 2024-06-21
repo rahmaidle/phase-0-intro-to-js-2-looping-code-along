2e59e4950be01a127750b66c87ff36f8fe0096ea
rahmaidle <rahmaidle254@gmail.com>
2024-06-21 14:16:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const result = [];
+    for (let i = 0; i < names.length; i++) {
+      result.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`);
+    }
+    return result;
+  }
+  
+  function countDown(n) {
+    for (let i = n; i >= 0; i--) console.log(i);
+  }
+  
+  export { writeCards, countDown };
